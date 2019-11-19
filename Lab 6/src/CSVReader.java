60d4334b190d05785bb59891b17d72d5f1fa13b7
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2019-11-19 03:10:29 +0100

    Lab 6 - Done

--- Lab 6/src/CSVReader.java
+++ Lab 6/src/CSVReader.java
@@ -1,6 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.Reader;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,6 +34,21 @@ public class CSVReader {
         if(hasHeader)parseHeader();
     }
 
+    public CSVReader(String filename, String delimiter) throws FileNotFoundException {
+        this(filename, delimiter, false);
+    }
+
+    public CSVReader(String filename) throws FileNotFoundException {
+        this(filename, ",", false);
+    }
+
+    public CSVReader(Reader reader, String delimiter, boolean hasHeader) {
+        this.reader = new BufferedReader(reader);
+        this.delimiter = delimiter;
+        this.hasHeader = hasHeader;
+        if(hasHeader)parseHeader();
+    }
+
     void parseHeader() {
         // wczytaj wiersz
         try {
@@ -38,7 +57,7 @@ public class CSVReader {
                 return;
             }
             // podziel na pola
-            String[] header = line.split(delimiter);
+            String[] header = line.split(delimiter + "(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
             // przetwarzaj dane w wierszu
             for (int i = 0; i < header.length; i++) {
                 columnLabels.add(header[i]);
@@ -57,7 +76,86 @@ public class CSVReader {
             return false;
         }
         if(tmp==null)return false;
-        current = tmp.split(delimiter);
+        current = tmp.split(delimiter + "(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
         return true;
+    }
+    List<String> getColumnLabels() {
+        return columnLabels;
+    }
+    int getRecordLength() {
+        return current.length;
+    }
+    boolean isMissing(int columnIndex) {
+        if(this.getRecordLength()<=columnIndex)return true;
+        if(current[columnIndex]!=null && current[columnIndex]!="")return false;
+        return true;
+    }
+    boolean isMissing(String columnLabel) {
+        if(columnLabelsToInt.size()==0 || columnLabelsToInt.get(columnLabel)==null) {
+            return true;
+        }
+        return isMissing(columnLabelsToInt.get(columnLabel));
+    }
+    String get(int columnIndex) {
+        if(current.length<=columnIndex ||current[columnIndex]==null)return "";
+        return current[columnIndex];
+    }
+    String get(String columnLabel) {
+        if(columnLabelsToInt.size()==0 || columnLabelsToInt.get(columnLabel)==null) {
+            return "";
+        }
+        return get(columnLabelsToInt.get(columnLabel));
+    }
+    int getInt(int columnIndex) {
+        if(columnIndex>=current.length)throw new RuntimeException("Out of bounds");
+        return Integer.parseInt(current[columnIndex]);
+    }
+    int getInt(String columnLabel) {
+        if(columnLabelsToInt.get(columnLabel)==null)throw new RuntimeException("Unknown Label");
+        return getInt(columnLabelsToInt.get(columnLabel));
+    }
+    long getLong(int columnIndex) {
+        if(columnIndex>=current.length)throw new RuntimeException("Out of bounds");
+        return Long.parseLong(current[columnIndex]);
+    }
+    long getLong(String columnLabel) {
+        if(columnLabelsToInt.get(columnLabel)==null)throw new RuntimeException("Unknown Label");
+        return getLong(columnLabelsToInt.get(columnLabel));
+    }
+    double getDouble(int columnIndex) {
+        if(columnIndex>=current.length)throw new RuntimeException("Out of bounds");
+        return Double.parseDouble(current[columnIndex]);
+    }
+    double getDouble(String columnLabel) {
+        if(columnLabelsToInt.get(columnLabel)==null)throw new RuntimeException("Unknown Label");
+        return getDouble(columnLabelsToInt.get(columnLabel));
+    }
+    LocalTime getTime(int columnIndex) throws RuntimeException {
+        return getTime(columnIndex, "HH:mm:ss");
+    }
+    LocalTime getTime(int columnIndex, String format) {
+        if(get(columnIndex)=="")throw new RuntimeException("Out of bounds");
+        return LocalTime.parse(get(columnIndex), DateTimeFormatter.ofPattern(format));
+    }
+    LocalTime getTime(String columnLabel) throws RuntimeException {
+        return getTime(columnLabel, "HH:mm:ss");
+    }
+    LocalTime getTime(String columnLabel, String format) {
+        if(columnLabelsToInt.get(columnLabel)==null)throw new RuntimeException("Unknown Label");
+        return getTime(columnLabelsToInt.get(columnLabel), format);
+    }
+    LocalDate getDate(int columnIndex) throws RuntimeException {
+        return getDate(columnIndex, "RR-mm-dd");
+    }
+    LocalDate getDate(int columnIndex, String format) {
+        if(get(columnIndex)=="")throw new RuntimeException("Out of bounds");
+        return LocalDate.parse(get(columnIndex), DateTimeFormatter.ofPattern(format));
+    }
+    LocalDate getDate(String columnLabel) throws RuntimeException {
+        return getDate(columnLabel, "RR-mm-dd");
+    }
+    LocalDate getDate(String columnLabel, String format) {
+        if(columnLabelsToInt.get(columnLabel)==null)throw new RuntimeException("Unknown Label");
+        return getDate(columnLabelsToInt.get(columnLabel), format);
     }
 }
--- Lab 6/src/main.java
+++ Lab 6/src/main.java
@@ -1,12 +1,89 @@
+import java.io.File;
+import java.io.Reader;
+import java.io.StringReader;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class main {
     public static void main(String[] args) {
-        CSVReader reader = new CSVReader("titanic-part.csv",",",true);
-        while(reader.next()){
-            int id = reader.getInt("PassengerId");
-            String name = reader.get("Name");
-            double fare = reader.getDouble("Fare");
+        //TEST 1
+        try {
+            //With header
+            System.out.println("With header\n");
+            CSVReader reader = new CSVReader("csv" + File.separator + "with-header.csv",";",true);
+            System.out.println(reader.getColumnLabels().toString());
+            while(reader.next()){
+                String id = reader.get("id");
+                String name = reader.get("imi�");
+                String lastname = reader.get("nazwisko");
+                String ulica = reader.get("ulica");
+                String nrdomu = reader.get("nrdomu");
+                String nrmieszkania = reader.get("nrmieszkania");
 
-            System.out.printf(Locale.Us,"%d %s %d",id, name, fare);
+                System.out.printf(Locale.US,"%s %s %s %s %s %s\n",id, name,lastname, ulica, nrdomu, nrmieszkania);
+            }
+            //Without header
+            System.out.println("\nNo header\n");
+            reader = new CSVReader("csv" + File.separator + "no-header.csv",";");
+            while(reader.next()){
+                String id = reader.get(0);
+                String name = reader.get(1);
+                String lastname = reader.get(2);
+                String ulica = reader.get(3);
+                String nrdomu = reader.get(4);
+                String nrmieszkania = reader.get(5);
+
+                System.out.printf(Locale.US,"%s %s %s %s %s %s\n",id, name,lastname, ulica, nrdomu, nrmieszkania);
+            }
+            //TEST 2
+            reader = new CSVReader("csv" + File.separator + "accelerator.csv",";",true);
+            System.out.println("\nTEST 2\n");
+            while(reader.next()){
+                String value1 = reader.get(0);
+                int value2 = reader.getInt("label");
+                double value3 = reader.getDouble(2);
+                long value4 = reader.getLong("time");
+                System.out.printf(Locale.US,"%s %d %f %d\n",value1,value2,value3,value4);
+            }
+            //TEST 3
+            reader = new CSVReader("csv" + File.separator + "missing-values.csv",";", true);
+            while (reader.next()) {
+                for (String columnLabel : reader.getColumnLabels()) {
+                    if(!reader.isMissing(columnLabel)) {
+                        System.out.printf("%s ", reader.get(columnLabel));
+                    }
+                }
+                System.out.println();
+            }
+            //TEST 4
+            reader = new CSVReader("csv" + File.separator + "with-header.csv",";", true);
+            try {
+                reader.next();
+                //System.out.println(reader.getInt("TEST"));
+                System.out.println(reader.getInt(20));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            //TEST 5
+            String text = "a,b,c\n123.4,567.8,91011.12";
+            reader = new CSVReader(new StringReader(text),",",true);
+            while (reader.next()){
+                for (String s : reader.current) {
+                    System.out.println(s);
+                }
+            }
+            //Titanic
+            System.out.println("\nTitanic\n");
+            reader = new CSVReader("csv" + File.separator + "titanic-part.csv",",",true);
+            while(reader.next()){
+                int id = reader.getInt("PassengerId");
+                String name = reader.get("Name");
+                double fare = reader.getDouble("Fare");
+                System.out.printf(Locale.US,"%d %s %f\n",id, name, fare);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
