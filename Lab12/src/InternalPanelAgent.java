2f600c414a948e9f7eecea60c23a824393168cbe
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2020-01-20 00:00:41 +0100

    Lab 12 DONE

---
+++ Lab12/src/InternalPanelAgent.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class InternalPanelAgent extends Thread {
+    static class InternalCall{
+        private final int toFloor;
+        InternalCall(int toFloor){
+            this.toFloor = toFloor;
+        }
+    }
+
+    InternalPanelAgent(ElevatorCar elevatorCar){
+        this.elevatorCar = elevatorCar;
+    }
+
+    BlockingQueue<InternalCall> input = new ArrayBlockingQueue<>(100);
+    ElevatorCar elevatorCar;
+
+    public void run(){
+        for(;;){
+            InternalPanelAgent.InternalCall ic = null;
+            try {
+                ic = input.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if(ic.toFloor==elevatorCar.getFloor())continue;
+            if(ic.toFloor > elevatorCar.getFloor()){
+                ElevatorStops.get().setLiftStopUp(ic.toFloor);
+            }else{
+                ElevatorStops.get().setLiftStopDown(ic.toFloor);
+            }
+        }
+    }
+}
